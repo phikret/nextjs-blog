23863c91949c8adcf1069634618a2a276f825df6
Fikret Pašović <kico_np@yahoo.com>
2022-08-31 11:35:52 +0200

    change format date

--- components/date.js
+++ components/date.js
@@ -3,6 +3,6 @@ import { parseISO, format } from 'date-fns';
 export default function Date({ dateString }) {
     const date = parseISO(dateString);
     return <time dateTime={dateString}>
-        {format(date, 'LLLL d, yyyy')}
+        {format(date, 'dd, MM, yyyy')}
     </time>
 }
